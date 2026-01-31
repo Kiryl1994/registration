d151e7f6675ea006fc914375b2cf976027c7f5df
Kiryl1994 <142154288+Kiryl1994@users.noreply.github.com>
2026-01-31 15:10:52 +0300

    Update script.js

--- 13. Страница регистрации с функционалом/scripts/script.js
+++ 13. Страница регистрации с функционалом/scripts/script.js
@@ -11,6 +11,8 @@
     let title = document.querySelector('h1');
     let subtitle = document.querySelector('h3');
     let linkBlock = document.querySelector('.link a');
+
+
     /* ===== 2. Запрет цифр в Full Name ===== */
     fullNameInput.addEventListener('input', function() {
         fullNameInput.value = fullNameInput.value.replace(/[0-9]/g, '');
@@ -27,57 +29,66 @@
     });
 
     /* ===== 5. Submit формы ===== */
-    form.addEventListener('submit', function signUpHandler() {
-        signUpHandler.preventDefault();
-        
-        // проверки пустых полей
-        if (!fullNameInput.value.trim()) {
-            alert('Заполните поле Full Name');
-            return;
-        } 
-        if (!usernameInput.value.trim()) {
-            alert('Заполните поле Your username');
-            return;
-        }
-        if (!emailInput.value.trim()) {
-          alert("Заполните поле E-mail");
-          return;
-        } else {
-          let emailPattern = /^\S+@\S+\.\S+$/;
-          if (!emailPattern.test(emailInput.value)) {
+
+    function signUpHandler(event) {
+    event.preventDefault();
+
+    // проверки пустых полей
+    if (!fullNameInput.value.trim()) {
+        alert('Заполните поле Full Name');
+        return;
+    } 
+
+    if (!usernameInput.value.trim()) {
+        alert('Заполните поле Your username');
+        return;
+    }
+
+    if (!emailInput.value.trim()) {
+        alert("Заполните поле E-mail");
+        return;
+    } else {
+        let emailPattern = /^\S+@\S+\.\S+$/;
+        if (!emailPattern.test(emailInput.value)) {
             alert("Введите корректный E-mail");
             return;
-          }
         }
-        if (!passwordInput.value.trim()) {
-            alert('Заполните поле Password');
-            return;
-        }
-        if (!repeatPasswordInput.value.trim()) {
-            alert('Заполните поле Repeat Password');
-            return;
-        }
+    }
 
-        // длина пароля
-        if (passwordInput.value.length < 8) {
-            alert('Пароль должен содержать не менее 8 символов');
-            return;
-        }
+    if (!passwordInput.value.trim()) {
+        alert('Заполните поле Password');
+        return;
+    }
 
-        // совпадение паролей
-        if (passwordInput.value !== repeatPasswordInput.value) {
-            alert('Пароли не совпадают');
-            return;
-        }
+    if (!repeatPasswordInput.value.trim()) {
+        alert('Заполните поле Repeat Password');
+        return;
+    }
 
-        // чекбокс
-        if (!checkbox.checked) {
+    // длина пароля
+    if (passwordInput.value.length < 8) {
+        alert('Пароль должен содержать не менее 8 символов');
+        return;
+    }
+
+    // совпадение паролей
+    if (passwordInput.value !== repeatPasswordInput.value) {
+        alert('Пароли не совпадают');
+        return;
+    }
+
+    // чекбокс
+    if (!checkbox.checked) {
         alert('Необходимо согласиться с условиями');
         return;
-        }
+    }
 
-        showModal();
-    });
+    showModal();
+}
+
+form.addEventListener('submit', signUpHandler);
+        
+        
 
     /* ===== 5. Попап ===== */
     function showModal() {
@@ -133,4 +144,152 @@
 
         alert(`Добро пожаловать, ${usernameInput.value}!`);
 
-    }
+    }}
+
+
+
+//     window.onload = function () {
+
+//     let form = document.querySelector('.form');
+//     let fullNameInput = document.getElementById('name');
+//     let usernameInput = document.getElementById('username');
+//     let emailInput = document.getElementById('mail');
+//     let passwordInput = document.getElementById('password');
+//     let repeatPasswordInput = document.getElementById('repeat');
+//     let checkbox = document.querySelector('.input__click');
+//     let button = document.querySelector('.button');
+//     let title = document.querySelector('h1');
+//     let subtitle = document.querySelector('h3');
+//     let linkBlock = document.querySelector('.link a');
+
+//     //Добавил новую переменную
+//     let isLoginPage = false;
+
+//     /* ===== 2. Запрет цифр в Full Name ===== */
+//     fullNameInput.addEventListener('input', function() {
+//         fullNameInput.value = fullNameInput.value.replace(/[0-9]/g, '');
+//     });
+
+//     /* ===== 3. Запрет . и , в username ===== */
+//     usernameInput.addEventListener('input', function() {
+//         usernameInput.value = usernameInput.value.replace(/[.,]/g, '');
+//     });
+
+//     /* ===== 4. Чекбокс ===== */
+//     checkbox.addEventListener('change', function() {
+//         console.log(checkbox.checked ? 'Согласен' : 'Не согласен');
+//     });
+
+//     form.addEventListener('submit', function(event) {
+//        event.preventDefault();
+
+//        if (isLoginPage) {
+//             loginHandler();
+//        } else {
+//             signUpHandler();
+//        }
+//     });
+
+//     function signUpHandler() {
+//         if (!fullNameInput.value.trim()) {
+//             alert('Заполните поле Full Name');
+//             return;
+//         } 
+//         if (!usernameInput.value.trim()) {
+//             alert('Заполните поле Your username');
+//             return;
+//         }
+
+//         if (!emailInput.value.trim()) {
+//           alert("Заполните поле E-mail");
+//           return;
+//         } else {
+//           let emailPattern = /^\S+@\S+\.\S+$/;
+//           if (!emailPattern.test(emailInput.value)) {
+//             alert("Введите корректный E-mail");
+//             return;
+//           }
+//         }
+
+//         if (!passwordInput.value.trim()) {
+//             alert('Заполните поле Password');
+//             return;
+//         }
+//         if (!repeatPasswordInput.value.trim()) {
+//             alert('Заполните поле Repeat Password');
+//             return;
+//         }
+
+//         // длина пароля
+//         if (passwordInput.value.length < 8) {
+//             alert('Пароль должен содержать не менее 8 символов');
+//             return;
+//         }
+
+//         // совпадение паролей
+//         if (passwordInput.value !== repeatPasswordInput.value) {
+//             alert('Пароли не совпадают');
+//             return;
+//         }
+
+//         // чекбокс
+//         if (!checkbox.checked) {
+//         alert('Необходимо согласиться с условиями');
+//         return;
+//         }
+
+//         showModal();
+//     }
+
+//      /* ===== 5. Попап ===== */
+//     function showModal() {
+//         const modal = document.createElement('div');
+//         modal.className = 'modal';
+//         modal.innerHTML = `
+//             <div class="modal__content">
+//                 <p>На вашу почту выслана ссылка, перейдите по ней, чтобы завершить регистрацию</p>
+//                 <button class="modal__btn">ОК</button>
+//             </div>
+//         `;
+//         document.body.append(modal);
+
+//         modal.querySelector('.modal__btn').addEventListener('click', function() {
+//             modal.remove();
+//             form.reset();
+//             switchToLogin();
+//         });
+//     }
+
+//     /* ===== 6. Переход на логин ===== */
+//     linkBlock.addEventListener('click', function(error) {
+//         error.preventDefault();
+//         switchToLogin();
+//     });
+
+//     function switchToLogin() {
+//         isLoginPage = true;
+//         title.textContent = 'Log in to the system';
+//         subtitle.remove();
+
+//         fullNameInput.closest('label').remove();
+//         emailInput.closest('label').remove();
+//         repeatPasswordInput.closest('label').remove();
+//         checkbox.closest('label').remove();
+//         linkBlock.remove();
+
+//         button.textContent = 'Sign In';
+
+//     }
+
+//     function loginHandler() {
+
+//         if (!usernameInput.value.trim()) {
+//             return alert('Заполните поле Your username');
+//         }
+//         if (!passwordInput.value.trim()) {
+//             return alert('Заполните поле Password');
+//         }
+
+//         alert(`Добро пожаловать, ${usernameInput.value}!`);
+//     }
+// }
